721af46f216e9b2ed8afac1fd3f6a6383642b5bf
Ivan Zyranau <ewancoder@gmail.com>
2024-06-17 04:42:27 +0400

    Move refactored classes to separate files

---
+++ FoulBot.Domain/BotDelayStrategy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace FoulBot.Domain;
+
+public interface IBotDelayStrategy
+{
+    /// <summary>
+    /// Delays processing message, creating a fictional "reading messages" pause.
+    /// </summary>
+    ValueTask DelayAsync();
+}
+
+public sealed class BotDelayStrategy : IBotDelayStrategy
+{
+    private readonly Random _random = new Random();
+    private readonly ILogger<BotDelayStrategy> _logger;
+
+    public BotDelayStrategy(ILogger<BotDelayStrategy> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask DelayAsync()
+    {
+        var delay = _random.Next(1, 100);
+        if (delay > 90)
+        {
+            delay = _random.Next(5000, 20000);
+        }
+        if (delay <= 90 && delay > 70)
+        {
+            delay = _random.Next(1500, 5000);
+        }
+        if (delay <= 70)
+        {
+            delay = _random.Next(200, 1200);
+        }
+
+        _logger.LogDebug("Initiating artificial delay of {Delay} milliseconds to read the message with 'Bot's eyes'.", delay);
+        await Task.Delay(delay);
+    }
+}
---
+++ FoulBot.Domain/FoulBotContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace FoulBot.Domain;
+
+public interface IFoulBotContext
+{
+    List<FoulMessage> GetUnprocessedSnapshot();
+    void Process(List<FoulMessage> context);
+}
+
+public sealed class FoulBotContext : IFoulBotContext
+{
+    private readonly ILogger<FoulBotContext> _logger;
+    private readonly IFoulChat _chat;
+    private string? _lastProcessedMessageId;
+
+    public FoulBotContext(
+        ILogger<FoulBotContext> logger,
+        IFoulChat chat)
+    {
+        _logger = logger;
+        _chat = chat;
+    }
+
+    public List<FoulMessage> GetUnprocessedSnapshot()
+    {
+        var snapshot = _chat.GetContextSnapshot();
+        if (_lastProcessedMessageId != null)
+        {
+            snapshot = snapshot
+                .SkipWhile(message => message.Id != _lastProcessedMessageId)
+                .Skip(1)
+                .ToList();
+
+            _logger.LogDebug("{LastProcessedId} is not null, checking only unprocessed messages. There are {Count} of them.", _lastProcessedMessageId, snapshot.Count);
+        }
+
+        return snapshot;
+    }
+
+    public void Process(List<FoulMessage> context)
+    {
+        _logger.LogDebug("Updating last processed ID: {PreviousLastProcessed} to {NewLastProcessed}.", _lastProcessedMessageId, context[^1].Id);
+        _lastProcessedMessageId = context[^1].Id;
+    }
+}
