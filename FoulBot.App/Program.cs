00cbb42581a01d207e7ae49cf50496c384181416
Ivan Zyranau <ewancoder@gmail.com>
2024-08-15 07:17:42 +0400

    Add a new console project

---
+++ FoulBot.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.CancelAsync();
+};
+
+try
+{
+    await Task.Delay(Timeout.Infinite, cts.Token);
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Exiting...");
+}
