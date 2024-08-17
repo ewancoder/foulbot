2fa6b88f44a6b255650808231fb77e0dc05c5cb3
Ivan Zyranau <ewancoder@gmail.com>
2024-08-18 01:28:27 +0400

    Improve unit tests for AllowedChatsProvider

--- FoulBot.Domain.Tests/AllowedChatsProviderTests.cs
+++ FoulBot.Domain.Tests/AllowedChatsProviderTests.cs
@@ -30,4 +30,35 @@ public class AllowedChatsProviderTests
 
         Assert.True(File.Exists(fileName));
     }
+
+    [Theory, AutoMoqData]
+    public async Task ShouldWorkConcurrently(string fileName)
+    {
+        using var sut = new AllowedChatsProvider(fileName);
+        var fixture = AutoMoqDataAttribute.CreateFixture();
+        var ids = fixture.CreateMany<FoulChatId>(100);
+
+        await Parallel.ForEachAsync(ids, new ParallelOptions
+        {
+            MaxDegreeOfParallelism = 10
+        }, (id, _) => sut.AllowChatAsync(id));
+
+        await Parallel.ForEachAsync(ids, new ParallelOptions
+        {
+            MaxDegreeOfParallelism = 10
+        }, async (id, _) => Assert.True(await sut.IsAllowedChatAsync(id)));
+    }
+
+    [Theory, AutoMoqData]
+    public async Task ShouldLoadExistingFile(string fileName, FoulChatId chatId)
+    {
+        using var sut = new AllowedChatsProvider(fileName);
+        Assert.False(await sut.IsAllowedChatAsync(chatId));
+
+        await sut.AllowChatAsync(chatId);
+        Assert.True(await sut.IsAllowedChatAsync(chatId));
+
+        using var sut2 = new AllowedChatsProvider(fileName);
+        Assert.True(await sut2.IsAllowedChatAsync(chatId));
+    }
 }
