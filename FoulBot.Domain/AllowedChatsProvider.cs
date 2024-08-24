c944bff4b23c786c916db9941091d365f90d2198
Ivan Zyranau <ewancoder@gmail.com>
2024-08-24 08:54:29 +0400

    Improve logging for AllowedChatsProvider

--- FoulBot.Domain/AllowedChatsProvider.cs
+++ FoulBot.Domain/AllowedChatsProvider.cs
@@ -41,7 +41,7 @@ public sealed class AllowedChatsProvider : IAllowedChatsProvider, IDisposable
 
     public async ValueTask AllowChatAsync(FoulChatId chatId)
     {
-        _logger.LogWarning("Allowing chat {ChatId}", chatId.Value);
+        _logger.LogWarning("Allowing chat {ChatId}", chatId);
 
         var allowedChats = await GetAllowedChatsAsync();
         allowedChats.TryAdd(GetKey(chatId), false);
@@ -51,7 +51,7 @@ public sealed class AllowedChatsProvider : IAllowedChatsProvider, IDisposable
 
     public async ValueTask DisallowChatAsync(FoulChatId chatId)
     {
-        _logger.LogWarning("Disallowing chat {ChatId}", chatId.Value);
+        _logger.LogWarning("Disallowing chat {ChatId}", chatId);
 
         var allowedChats = await GetAllowedChatsAsync();
         allowedChats.TryRemove(GetKey(chatId), out _);
@@ -63,17 +63,18 @@ public sealed class AllowedChatsProvider : IAllowedChatsProvider, IDisposable
     {
         var allowedChats = await GetAllowedChatsAsync();
 
-        var serialized = JsonSerializer.Serialize(
-            allowedChats.Keys.Select(chatId => chatId));
+        var serialized = JsonSerializer.Serialize(allowedChats.Keys);
 
         await _lock.WaitAsync();
         try
         {
+            _logger.LogTrace("Acquired lock for saving allowed chats");
             _logger.LogDebug("Saving list of allowed chats: {SerializedAllowedChats}", serialized);
             await File.WriteAllTextAsync(_fileName, serialized);
         }
         finally
         {
+            _logger.LogTrace("Releasing lock for saving allowed chats");
             _lock.Release();
         }
     }
@@ -91,18 +92,26 @@ public sealed class AllowedChatsProvider : IAllowedChatsProvider, IDisposable
         await _lock.WaitAsync();
         try
         {
+            _logger.LogTrace("Acquired lock for initially reading allowed chats");
 #pragma warning disable CA1508 // False positive: It is updated below.
             if (_allowedChats != null)
+            {
+                _logger.LogTrace("Allowed chats were populated by another thread. Skipping");
                 return _allowedChats;
+            }
 #pragma warning restore CA1508
 
             if (!File.Exists(_fileName))
+            {
+                _logger.LogTrace("Allowed chats file was not created. Creating");
                 await File.WriteAllTextAsync(_fileName, "[]");
+            }
 
             var fileContent = await File.ReadAllTextAsync(_fileName);
             var chats = JsonSerializer.Deserialize<string[]>(fileContent)?.Distinct()
                 ?? throw new InvalidOperationException("Failed to deserialize allowed chats.");
 
+            _logger.LogDebug("Finished reading allowed chats from file");
             _allowedChats = new ConcurrentDictionary<string, bool>(
                 chats.Select(chat => new KeyValuePair<string, bool>(GetKey(new(chat)), false)));
 
@@ -110,6 +119,7 @@ public sealed class AllowedChatsProvider : IAllowedChatsProvider, IDisposable
         }
         finally
         {
+            _logger.LogTrace("Releasing lock for initially reading allowed chats");
             _lock.Release();
         }
     }
