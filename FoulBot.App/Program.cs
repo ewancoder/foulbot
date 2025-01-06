b9245c686a9b325cf8b885194ac1403f056d9024
Ivan Zyranau <ewancoder@gmail.com>
2025-01-07 01:31:51 +0400

    Fix graceful shutdown

--- FoulBot.App/Program.cs
+++ FoulBot.App/Program.cs
@@ -1,13 +1,28 @@
 ﻿global using FoulBot.Domain;
 global using FoulBot.Infrastructure;
-
 using FoulBot.App;
 
 using var cts = new CancellationTokenSource();
+
+AppDomain.CurrentDomain.ProcessExit += (_, e)
+    => GracefullyShutdown().GetAwaiter().GetResult();
+
 Console.CancelKeyPress += (_, e) =>
 {
+    GracefullyShutdown().GetAwaiter().GetResult();
     e.Cancel = true;
-    cts.CancelAsync();
 };
+
+async Task GracefullyShutdown()
+{
+    Console.WriteLine("Gracefully shutting down.");
+
+    await cts.CancelAsync();
+
+    // Wait an arbitrary amount of time hoping our cleanup logic will be done by then.
+    await Task.Delay(TimeSpan.FromSeconds(5));
+
+    Console.WriteLine("Gracefully shut down the application.");
+}
 
 await FoulBotServer.StartAsync(cts.Token);
