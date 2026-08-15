8a81a7d2c8227fcbc263420ae849a6862b273ac1
Ivan Zyranau <ewancoder@gmail.com>
2026-08-16 00:30:05 +0400

    blabla

---
+++ FoulBot.Api/FoulBot.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace FoulBot.Api
+{
+    public interface IFoulBot
+    {
+        ValueTask HandleUpdateAsync(ITelegramBotClient botClient, Update update);
+    }
+
+    public sealed class FoulBot
+    {
+        private readonly string _chat;
+        private readonly string _botName;
+        private readonly bool _isDebug;
+        private readonly IEnumerable<string> _keyWords;
+        private readonly bool _listenToConversation;
+        private readonly IFoulAIClient _foulAIClient;
+        private readonly int _messagesBetweenAudio;
+        private readonly bool _useConsoleInsteadOfTelegram;
+        private readonly Random _random = new Random();
+        private readonly int _replyEveryMessages;
+        private readonly Task _replyByTime;
+        private ITelegramBotClient? _cachedBotClient;
+        private int _audioCounter = 0;
+        private int _replyEveryMessagesCounter = 0;
+
+        public FoulBot(
+            ITelegramBotClient botClient,
+            string chat,
+            string botName,
+            bool isDebug,
+            IEnumerable<string> keyWords,
+            bool listenToConversation,
+            IFoulAIClient foulAIClient,
+            int messagesBetweenAudio,
+            int replyEveryMessages,
+            bool useConsoleInsteadOfTelegram)
+        {
+            _chat = chat;
+            _botName = botName;
+            _isDebug = isDebug;
+            _keyWords = keyWords;
+            _listenToConversation = listenToConversation;
+            _foulAIClient = foulAIClient;
+            _messagesBetweenAudio = messagesBetweenAudio;
+            _cachedBotClient = botClient;
+            _useConsoleInsteadOfTelegram = useConsoleInsteadOfTelegram;
+            _replyEveryMessages = replyEveryMessages;
+
+            _replyByTime = Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(20));
+                await SendPersonalMessageAsync();
+
+                while (true)
+                {
+                    await Task.Delay(TimeSpan.FromMinutes(_random.Next(20, 120)));
+                    await SendPersonalMessageAsync();
+                }
+            });
+        }
+
+        public async ValueTask HandleUpdateAsync(ITelegramBotClient botClient, Update update)
+        {
+            _cachedBotClient = botClient;
+
+            if (!UpdateHasMessage(update))
+                return;
+
+            if (!NeedToReply(update))
+            {
+                if (_listenToConversation)
+                    await _foulAIClient.AddToContextAsync(update.Message!.From!.FirstName, update.Message!.Text!);
+
+                return;
+            }
+
+            if (_messagesBetweenAudio > 0)
+                _audioCounter++;
+            if (_replyEveryMessages > 0)
+                _replyEveryMessagesCounter++;
+
+            var chatId = update.Message!.Chat.Id;
+            if (chatId.ToString() != _chat)
+                throw new InvalidOperationException("Chat ID doesn't match."); // Log this.
+
+            if (_audioCounter > _messagesBetweenAudio)
+            {
+                _audioCounter = 0;
+
+                if (_useConsoleInsteadOfTelegram)
+                {
+                    Console.WriteLine(update.Message!.Text!);
+                }
+                else
+                {
+                    var stream = await _foulAIClient.GetAudioResponseAsync(update.Message!.From!.FirstName, update.Message!.Text!);
+                    await botClient.SendVoiceAsync(chatId, InputFile.FromStream(stream));
+                }
+
+                return;
+            }
+
+            var text = await _foulAIClient.GetTextResponseAsync(update.Message!.From!.FirstName, update.Message!.Text!);
+
+            if (_useConsoleInsteadOfTelegram)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(chatId, _isDebug ? $"{text.Item1} ({text.Item2})" : text.Item1);
+            }
+        }
+
+        private async Task SendPersonalMessageAsync()
+        {
+            if (!_listenToConversation || _cachedBotClient == null)
+                return;
+
+            var text = await _foulAIClient.GetPersonalTextResponseAsync();
+
+            if (_useConsoleInsteadOfTelegram)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                await _cachedBotClient.SendTextMessageAsync(_chat, _isDebug ? $"{text.Item1} ({text.Item2})" : text.Item1);
+            }
+        }
+
+        private bool UpdateHasMessage(Update update)
+        {
+            if (update.Type != UpdateType.Message)
+                return false;
+
+            if (update.Message?.Text == null)
+                return false;
+
+            if (update.Message.From == null || update.Message.From.FirstName == null)
+                return false;
+
+            return true;
+        }
+
+        private bool NeedToReply(Update update)
+        {
+            if (update.Message?.ReplyToMessage?.From?.Username == _botName)
+                return true;
+
+            if (update.Message?.Text != null)
+            {
+                if (_keyWords.Any(keyWord => update.Message.Text.ToLowerInvariant().Contains(keyWord.ToLowerInvariant().Trim())))
+                    return true;
+            }
+
+            if (_replyEveryMessages > 0)
+                _replyEveryMessagesCounter++;
+
+            if (_replyEveryMessagesCounter > _replyEveryMessages && _random.Next(0, 10) > 7)
+            {
+                _replyEveryMessagesCounter = 0;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ FoulBot.Api/FoulBotGateway.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace FoulBot.Api
+{
+    public sealed class FoulBotGatewayFactory
+    {
+        private readonly string _aiApiKey;
+        private readonly bool _isDebugging;
+        private readonly bool _useConsoleInsteadOfTelegram;
+
+        public FoulBotGatewayFactory(
+            string aiApiKey,
+            bool isDebugging,
+            bool useConsoleInsteadOfTelegram)
+        {
+            _aiApiKey = aiApiKey;
+            _isDebugging = isDebugging;
+            _useConsoleInsteadOfTelegram = useConsoleInsteadOfTelegram;
+        }
+
+        public FoulBotGateway CreateFoulBot(
+            string botApiKey, string botName, IEnumerable<string> keyWords,
+            string mainDirective,
+            bool listenToConversation = true,
+            int maxMessagesInContext = 20,
+            int messagesBetweenAudio = 0)
+        {
+            return new FoulBotGateway(
+                _isDebugging,
+                botApiKey,
+                _aiApiKey,
+                botName,
+                keyWords,
+                listenToConversation,
+                maxMessagesInContext,
+                mainDirective,
+                messagesBetweenAudio,
+                _useConsoleInsteadOfTelegram);
+        }
+    }
+
+    public sealed class FoulBotGateway : IUpdateHandler
+    {
+        private readonly List<string> _cachedChats = new List<string>();
+        private readonly DateTime _start = DateTime.UtcNow;
+        private readonly bool _isDebugging;
+        private readonly string _aiApiKey;
+        private readonly ITelegramBotClient _client;
+        private readonly string _botName;
+        private readonly ConcurrentDictionary<string, FoulBot> _botPool
+            = new ConcurrentDictionary<string, FoulBot>();
+        private readonly IEnumerable<string> _keyWords;
+        private readonly bool _listenToConversation;
+        private readonly int _maxMessagesInContext;
+        private readonly int _messagesBetweenAudio;
+        private readonly bool _useConsoleInsteadOfTelegram;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private string _mainDirective;
+
+        public FoulBotGateway(
+            bool isDebugging,
+            string botApiKey,
+            string aiApiKey,
+            string botName,
+            IEnumerable<string> keyWords,
+            bool listenToConversation,
+            int maxMessagesInContext,
+            string mainDirective,
+            int messagesBetweenAudio,
+            bool useConsoleInsteadOfTelegram)
+        {
+            _isDebugging = isDebugging;
+            _aiApiKey = aiApiKey;
+            _botName = botName;
+            _keyWords = keyWords;
+            _maxMessagesInContext = maxMessagesInContext;
+            _listenToConversation = listenToConversation;
+            _mainDirective = mainDirective;
+            _messagesBetweenAudio = messagesBetweenAudio;
+            _useConsoleInsteadOfTelegram = useConsoleInsteadOfTelegram;
+            _client = new TelegramBotClient(botApiKey);
+            _client.StartReceiving(this, cancellationToken: _cts.Token);
+            TryCachingTheChat(null);
+            foreach (var chat in _cachedChats)
+            {
+                _botPool.GetOrAdd(chat, chat => new FoulBot(
+                    _client,
+                    chat,
+                    _botName,
+                    _isDebugging,
+                    _keyWords,
+                    _listenToConversation,
+                    new FoulAIClient(_aiApiKey, _maxMessagesInContext, _mainDirective),
+                    _messagesBetweenAudio,
+                    20,
+                    _useConsoleInsteadOfTelegram));
+            }
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // 10 seconds cold start.
+            if (DateTime.UtcNow < _start.AddSeconds(10))
+                return;
+
+            var chat = update.Message?.Chat?.Id.ToString();
+            if (chat == null)
+                return;
+
+            TryCachingTheChat(chat);
+
+            var bot = _botPool.GetOrAdd(chat, chat => new FoulBot(
+                _client,
+                chat,
+                _botName,
+                _isDebugging,
+                _keyWords,
+                _listenToConversation,
+                new FoulAIClient(_aiApiKey, _maxMessagesInContext, _mainDirective),
+                _messagesBetweenAudio,
+                20,
+                _useConsoleInsteadOfTelegram));
+
+            await bot.HandleUpdateAsync(botClient, update);
+        }
+
+        public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // TODO: Log the error.
+            return Task.CompletedTask;
+        }
+
+        public void Stop()
+        {
+            _cts.Cancel();
+        }
+
+        public BotInfo ToBotInfo()
+        {
+            return new BotInfo(_botName, _mainDirective);
+        }
+
+        public void ResetWithNewDirective(string newMainDirective)
+        {
+            _mainDirective = newMainDirective;
+            _botPool.Clear();
+        }
+
+        private void TryCachingTheChat(string? chat)
+        {
+            try
+            {
+                if (_cachedChats.Count == 0 && System.IO.File.Exists($"{_botName}.chats"))
+                    _cachedChats.AddRange(
+                        JsonSerializer.Deserialize<IEnumerable<string>>(
+                            System.IO.File.ReadAllText($"{_botName}.chats")));
+
+                if (chat != null && !_cachedChats.Contains(chat))
+                {
+                    _cachedChats.Add(chat);
+                    System.IO.File.WriteAllText($"{_botName}.chats", JsonSerializer.Serialize(_cachedChats));
+                }
+            } catch
+            {
+                Console.WriteLine("Could not cache/read chats.");
+            }
+        }
+    }
+}
